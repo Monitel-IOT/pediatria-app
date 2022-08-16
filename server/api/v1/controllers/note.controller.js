a563ebc525f48b15e5b6ee01927cd0eba10a2753
HenryCodeT <120686@unsaac.edu.pe>
2022-08-15 22:36:34 -0500

    add: middle ware and changes

--- server/api/v1/controllers/note.controller.js
+++ server/api/v1/controllers/note.controller.js
@@ -1,7 +1,12 @@
-const getHolaMundo = (request, response) => {
+const getHolaMundo = (request, response, next) => {
   response.send('hola mundo xdd');
+};
+
+const errorHandler = (request, response, next) => {
+  throw new Error('My first Sentry error!');
 };
 
 module.exports = {
   getHolaMundo,
+  errorHandler,
 };
--- server/api/v1/routes/routes.js
+++
@@ -1,9 +0,0 @@
-// Routers
-const NoteRouter = require('./notes.routes');
-
-function routes(app) {
-  // Routes
-  app.use('/api/notes', NoteRouter);
-}
-
-module.exports = routes;
