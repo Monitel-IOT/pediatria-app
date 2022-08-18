c1caef34cde58826aa97d49d78946c7bcc3cdede
torvic <130111@unsaac.edu.pe>
2022-08-17 22:59:04 -0500

    Feat: Add create user & read all users enpoint

--- server/api/v1/index.js
+++ server/api/v1/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 
 const AtencionRouter = require('./routes/atencion.routes');
+const userRouter = require('./routes/user.routes');
 
 const router = express.Router();
 
 router.use('/atencion', AtencionRouter);
+router.use('/user', userRouter);
+
 module.exports = router;
