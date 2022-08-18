7af5679f55a2605790739c480141e734fec19f3e
HenryCodeT <120686@unsaac.edu.pe>
2022-08-17 20:35:57 -0500

    request create paciente

--- server/api/v1/models/atencion.model.js
+++ server/api/v1/models/atencion.model.js
@@ -1,66 +1,60 @@
 const mongoose = require('mongoose');
 
 const atencion = new mongoose.Schema({
-  AtencionId: {
-    type: String,
-    required: [true, 'Es requerido'],
-  },
-  FechaAtencion: Date,
-  // Peso: String,
-  // Talla: String,
-  // Edad: String,
-  // PerimetroEncefalico: String,
-  // IndiceMasaCop: String,
+  AtencionId: String,
+  FechaAtencion: String,
+  Peso: String,
+  Talla: String,
+  Edad: String,
+  PerimetroEncefalico: String,
+  IndiceMasaCop: String,
   DiagnosticoProl: [{
-    Diagnostico: {
-      type: String,
-      required: [true, 'Es requerido'],
-    },
-    Edad: Number,
+    Diagnostico: String,
+    Edad: String,
   }],
-  // CaracteristicasAtencion: {
-  //   TiempoEnfer: String,
-  //   Relato: String,
-  //   Reevaluacion: String,
-  // },
-  // FuncionesVitales: {
-  //   FR: String,
-  //   FC: String,
-  //   T: String,
-  //   Saturacion: String,
-  // },
-  // Diagnostico: [
-  //   {
-  //     NombreDiagnostico: String,
-  //   },
-  // ],
-  // TratamientoLargo: [
-  //   {
-  //     NombreTratamientoLargo: String,
-  //     Estado: String,
-  //     FechaSuspencion: String,
-  //   },
-  // ],
-  // Tratamiento: [
-  //   {
-  //     NombreTratamiento: String,
-  //   },
-  // ],
-  // ExamenesAuxiliares: [
-  //   {
-  //     Examen: String,
-  //   },
-  // ],
-  // Percentiles: {
-  //   PercentilPeso: String,
-  //   PercentilTalla: String,
-  //   PercentilMC: String,
-  // },
-  // Estado: String,
-  // FechaCreacion: String,
-  // FechaModificacion: String,
+  CaracteristicasAtencion: {
+    TiempoEnfer: String,
+    Relato: String,
+    Reevaluacion: String,
+  },
+  FuncionesVitales: {
+    FR: String,
+    FC: String,
+    T: String,
+    Saturacion: String,
+  },
+  Diagnostico: [
+    {
+      NombreDiagnostico: String,
+    },
+  ],
+  TratamientoLargo: [
+    {
+      NombreTratamientoLargo: String,
+      Estado: String,
+      FechaSuspencion: String,
+    },
+  ],
+  Tratamiento: [
+    {
+      NombreTratamiento: String,
+    },
+  ],
+  ExamenesAuxiliares: [
+    {
+      Examen: String,
+    },
+  ],
+  Percentiles: {
+    PercentilPeso: String,
+    PercentilTalla: String,
+    PercentilMC: String,
+  },
+  Estado: String,
+  FechaCreacion: String,
+  FechaModificacion: String,
 
-});
+}, { timestamps: true });
 
 const Atencion = mongoose.model('Atencion', atencion);
 
