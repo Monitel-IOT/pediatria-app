4195f67ca74bc911ac05c432c51adf192a5ba97e
HenryCodeT <120686@unsaac.edu.pe>
2022-08-28 13:35:55 -0500

    añadiendo a la lista de usuarios al crear un paciente

--- server/api/v1/controllers/patient.controller.js
+++ server/api/v1/controllers/patient.controller.js
@@ -1,4 +1,6 @@
+/* eslint-disable no-underscore-dangle */
 const { getAllPatients, createNewPatient } = require('../services/patient.service');
+const { addNewPatientToUser } = require('../services/user.service');
 
 const getAllPatientHandler = async (req, res, next) => {
   try {
@@ -11,16 +13,35 @@ const getAllPatientHandler = async (req, res, next) => {
     next(error);
   }
 };
-/*prueba*/
+/* prueba */
 const createNewPatientHandler = async (req, res, next) => {
   try {
     const patient = req.body;
+    console.log(patient);
     const newPatient = await createNewPatient(patient);
     res.status(200).json({
       data: newPatient,
       status: 'OK',
     });
   } catch (error) {
+    console.log({ error });
+    next(error);
+  }
+};
+
+const createPatientToUserHandler = async (req, res, next) => {
+  try {
+    const patient = req.body;
+    const userId = req.params.id;
+
+    const newPatient = await createNewPatient(patient);
+    const updatedUser = await addNewPatientToUser(userId, newPatient._id);
+    res.status(200).json({
+      data: updatedUser,
+      status: 'OK',
+    });
+  } catch (error) {
+    console.log({ error });
     next(error);
   }
 };
@@ -28,4 +49,5 @@ const createNewPatientHandler = async (req, res, next) => {
 module.exports = {
   getAllPatientHandler,
   createNewPatientHandler,
+  createPatientToUserHandler,
 };
--- server/api/v1/services/patient.service.js
+++ server/api/v1/services/patient.service.js
@@ -1,11 +1,10 @@
 const Patient = require('../models/patient.model');
 
 async function createNewPatient(patient) {
-  let newPatient = await Patient.findOne({ numberHC: patient.numberHC }) || null;
+  let newPatient = await Patient.findOne({ NumberHC: patient.NumberHC }) || null;
+  console.log(newPatient);
   if (newPatient !== null) {
-    return {
-      msg: 'The patient already exists',
-    };
+    throw new Error('The patient already exists');
   }
   newPatient = new Patient(patient);
   await newPatient.save();
@@ -23,13 +22,14 @@ async function getPatientById(id) {
 }
 
 async function updatePatient(id, patient) {
+  console.log(id, patient);
   const newPatient = await Patient.findByIdAndUpdate(id, patient, { new: true });
   return newPatient;
 }
 
 async function getAllPatients() {
   const patients = await Patient.find({}).sort({ date: -1 });
-  return users;
+  return patients;
 }
 
 module.exports = {
