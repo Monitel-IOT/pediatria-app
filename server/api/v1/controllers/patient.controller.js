d1d6485408e532a7604ff9c84de24f687b3d9d5c
HenryCodeT <120686@unsaac.edu.pe>
2022-09-11 12:05:19 -0500

    retrieve list of patients

--- server/api/v1/controllers/patient.controller.js
+++ server/api/v1/controllers/patient.controller.js
@@ -1,26 +1,11 @@
 /* eslint-disable no-underscore-dangle */
 const { getAllPatients, createNewPatient } = require('../services/patient.service');
-const { addNewPatientToUser } = require('../services/user.service');
 
 const getAllPatientHandler = async (req, res, next) => {
   try {
     const patients = await getAllPatients();
     res.status(200).json({
       data: patients,
-      status: 'OK',
-    });
-  } catch (error) {
-    next(error);
-  }
-};
-/* prueba */
-const createNewPatientHandler = async (req, res, next) => {
-  try {
-    const patient = req.body;
-    console.log(patient);
-    const newPatient = await createNewPatient(patient);
-    res.status(200).json({
-      data: newPatient,
       status: 'OK',
     });
   } catch (error) {
@@ -34,20 +19,17 @@ const createPatientToUserHandler = async (req, res, next) => {
     const patient = req.body;
     const userId = req.params.id;
 
-    const newPatient = await createNewPatient(patient);
-    const updatedUser = await addNewPatientToUser(userId, newPatient._id);
+    const newPatient = await createNewPatient(userId, patient);
     res.status(200).json({
-      data: updatedUser,
+      data: newPatient,
       status: 'OK',
     });
   } catch (error) {
-    console.log({ error });
     next(error);
   }
 };
 
 module.exports = {
   getAllPatientHandler,
-  createNewPatientHandler,
   createPatientToUserHandler,
 };
