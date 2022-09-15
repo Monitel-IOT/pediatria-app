c25966371e75b714ee647fed8ad3f0e0b3647e06
HenryCodeT <120686@unsaac.edu.pe>
2022-09-14 20:46:27 -0500

    crud user with state

--- server/api/v1/routes/user.routes.js
+++ server/api/v1/routes/user.routes.js
@@ -4,6 +4,9 @@ const userController = require('../controllers/user.controller');
 const router = express.Router();
 
 router.get('/:id/patients', userController.getAllPatientsByUserIdHandler);
+router.delete('/:id', userController.deleteUserByIdHandler);
+router.get('/:id', userController.getUserByIdHandler);
+router.put('/:id', userController.updateUserByIdHandler);
 router.get('', userController.getAllUsersHandler);
 router.post('', userController.createNewUserHandler);
 
