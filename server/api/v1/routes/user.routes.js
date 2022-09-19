e034a61b63a5ae5f39fd88106fc5dc456b63466e
torvic <130111@unsaac.edu.pe>
2022-09-18 21:39:46 -0500

    Feat: Add firebase SDK to the project

--- server/api/v1/routes/user.routes.js
+++ server/api/v1/routes/user.routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const authenticate = require('../../../middleware/authenticate');
 const userController = require('../controllers/user.controller');
 
 const router = express.Router();
@@ -7,7 +8,7 @@ router.get('/:id/patients', userController.getAllPatientsByUserIdHandler);
 router.delete('/:id', userController.deleteUserByIdHandler);
 router.get('/:id', userController.getUserByIdHandler);
 router.put('/:id', userController.updateUserByIdHandler);
-router.get('', userController.getAllUsersHandler);
+router.get('', authenticate, userController.getAllUsersHandler);
 router.post('', userController.createNewUserHandler);
 
 module.exports = router;
