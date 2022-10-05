0006aceaa94b0bf39a1e05da8082114e466445f9
Cristian Benites Condori <78431820+cb3ndev@users.noreply.github.com>
2022-10-05 11:35:56 -0500

    feat: API delete appointment by Id

--- server/api/v1/routes/appointment.routes.js
+++ server/api/v1/routes/appointment.routes.js
@@ -7,5 +7,6 @@ const AppointmentController = require('../controllers/appointment.controller');
 AppointmentRouter.post('', AppointmentController.createNewAppointmentHandler);
 AppointmentRouter.get('/:id', AppointmentController.getAppointmentByIdHandler);
 AppointmentRouter.put('/:id', AppointmentController.updateAppointmentHandler);
+AppointmentRouter.delete('/:id', AppointmentController.deleteAppointmentHandler);
 
 module.exports = AppointmentRouter;
