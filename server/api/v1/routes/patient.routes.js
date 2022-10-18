4212a0f57144656faf541609e7a607065d8859c0
HenryCodeT <120686@unsaac.edu.pe>
2022-10-17 19:59:33 -0500

    obtener la lista de atenciones por id de paciente

--- server/api/v1/routes/patient.routes.js
+++ server/api/v1/routes/patient.routes.js
@@ -7,5 +7,6 @@ router.get('', patientController.getAllPatientHandler);
 router.post('/:id', patientController.createPatientToUserHandler);
 router.get('/:id', patientController.getPatientByIdHandler);
 router.delete('/:id', patientController.deletePatientHandler);
+router.get('/:id/appointments', patientController.getAllAppointmentsByPatientHandler);
 
 module.exports = router;
