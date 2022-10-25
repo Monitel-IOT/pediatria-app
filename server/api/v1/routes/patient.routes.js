3dace9336b2332d70588b26dbfe8bb59da28a427
torvic <130111@unsaac.edu.pe>
2022-10-25 17:03:36 -0500

    Feat: Add authenticate middleware to patient endpoint

--- server/api/v1/routes/patient.routes.js
+++ server/api/v1/routes/patient.routes.js
@@ -4,7 +4,7 @@ const patientController = require('../controllers/patient.controller');
 const router = express.Router();
 
 router.get('', patientController.getAllPatientHandler);
-router.post('/:id', patientController.createPatientToUserHandler);
+router.post('', patientController.createPatientToUserHandler);
 router.get('/:id', patientController.getPatientByIdHandler);
 router.delete('/:id', patientController.deletePatientHandler);
 router.get('/:id/appointments', patientController.getAllAppointmentsByPatientHandler);
