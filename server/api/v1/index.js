973de9c9240e563f5a780487bbb98ed7e0046a71
torvic <130111@unsaac.edu.pe>
2022-10-25 17:03:36 -0500

    Feat: Add authenticate middleware to patient endpoint

--- server/api/v1/index.js
+++ server/api/v1/index.js
@@ -3,10 +3,11 @@ const express = require('express');
 const AppointmentRouter = require('./routes/appointment.routes');
 const userRouter = require('./routes/user.routes');
 const patientRouter = require('./routes/patient.routes');
+const authenticate = require('../../middleware/authenticate');
 
 const router = express.Router();
 
 router.use('/appointment', AppointmentRouter);
 router.use('/user', userRouter);
-router.use('/patient', patientRouter);
+router.use('/patient', authenticate, patientRouter);
 module.exports = router;
--- server/api/v1/routes/user.routes.js
+++ server/api/v1/routes/user.routes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const authenticate = require('../../../middleware/authenticate');
+// const authenticate = require('../../../middleware/authenticate');
 const userController = require('../controllers/user.controller');
 
 const router = express.Router();
@@ -8,7 +8,7 @@ router.get('/:id/patients', userController.getAllPatientsByUserIdHandler);
 router.delete('/:id', userController.deleteUserByIdHandler);
 router.get('/:id', userController.getUserByIdHandler);
 router.put('/:id', userController.updateUserByIdHandler);
-router.get('', authenticate, userController.getAllUsersHandler);
+router.get('', userController.getAllUsersHandler);
 router.post('', userController.createNewUserHandler);
 router.get('/firebase/:firebaseId', userController.getUserByFirebaseIdHandler);
 
