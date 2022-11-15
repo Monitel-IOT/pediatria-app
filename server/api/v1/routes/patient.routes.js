f342a0c668129691f44dbff0e47bf3e792c49930
HenryCodeT <120686@unsaac.edu.pe>
2022-11-14 22:09:30 -0500

    actualizando paciente

--- server/api/v1/routes/patient.routes.js
+++ server/api/v1/routes/patient.routes.js
@@ -8,5 +8,6 @@ router.post('', patientController.createPatientToUserHandler);
 router.get('/:id', patientController.getPatientByIdHandler);
 router.delete('/:id', patientController.deletePatientHandler);
 router.get('/:id/appointments', patientController.getAllAppointmentsByPatientHandler);
+router.put('/:id', patientController.editPatientToUserHandler);
 
 module.exports = router;
