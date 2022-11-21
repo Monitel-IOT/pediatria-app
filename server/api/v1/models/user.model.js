84d705e539def3f8decc08156e53d0ccdc6ccecd
torvic <torvic02st96@gmail.com>
2022-11-21 15:49:57 -0500

    Feat: Add new validations to authenticate middleware

--- server/api/v1/models/user.model.js
+++ server/api/v1/models/user.model.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema(
   {
-    name: { type: String, required: true },
+    name: { type: String, required: false },
     surname: { type: String, required: false },
     email: { type: String, required: true, unique: true },
     dni: { type: String },
